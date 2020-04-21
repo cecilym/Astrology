edb66698e6da58471e68776a3d6d468df78fe75c
C M <cecilymadanes@gmail.com>
2020-04-21 09:40:08 -0400

    New format
    Major change, need to update categories

--- script.js
+++ script.js
@@ -1,83 +1,56 @@
-$( document ).ready(function() {
-  console.log( 'ready!' );
+$(document).ready(function () {
+  console.log( 'ready2!' );
+  
+  var $filterCheckboxes = $('input[type="checkbox"]');
 
-});
+  $filterCheckboxes.on('change', function() {
 
+    var selectedFilters = {};
 
-filterSelection("all")
-function filterSelection(c) {
-  var x, i;
-  //x = document.getElementsByClassName("filterDiv");
-  x = $( '.filterDiv' ); // select all elements with a class of 'filterDiv'
-  if (c == "all") c = "";
-  // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
-  for (i = 0; i < x.length; i++) {
-    w3RemoveClass(x[i], "show");
-    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
-  }
-}
+    $filterCheckboxes.filter(':checked').each(function() {
 
-// Show filtered elements
-function w3AddClass(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    if (arr1.indexOf(arr2[i]) == -1) {
-      element.className += " " + arr2[i];
-    }
-  }
-}
+      if (!selectedFilters.hasOwnProperty(this.name)) {
+        selectedFilters[this.name] = [];
+      }
 
-// Hide elements that are not selected
-function w3RemoveClass(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    while (arr1.indexOf(arr2[i]) > -1) {
-      arr1.splice(arr1.indexOf(arr2[i]), 1);
-    }
-  }
-  element.className = arr1.join(" ");
-}
+      selectedFilters[this.name].push(this.value);
 
-// Add active class to the current control button (highlight it)
-var btns = $( '.btn' ); 
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function() {
-    var btnText = $(this).text();
-    console.log(btnText);
-    $('#showClasses').replaceWith('<p id="showClasses">' + btnText + '</p>');
-    var current = $( '.active' );
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
-  });
-}
+    });
 
+    // create a collection containing all of the filterable elements
+    var $filteredResults = $('.herb');
 
-/*
-$('button').on( "click", function() {
-    var btnText = $(this).text();
-    console.log(btnText);
-    $('#showClasses').replaceWith(btnText);
+    // loop over the selected filter name -> (array) values pairs
+    $.each(selectedFilters, function(name, filterValues) {
+
+      // filter each .herb element
+      $filteredResults = $filteredResults.filter(function() {
+
+        var matched = false,
+          currentFilterValues = $(this).data('category').split(' ');
+
+        // loop over each category value in the current .herb's data-category
+        $.each(currentFilterValues, function(_, currentFilterValue) {
+
+          // if the current category exists in the selected filters array
+          // set matched to true, and stop looping. as we're ORing in each
+          // set of filters, we only need to match once
+
+          if ($.inArray(currentFilterValue, filterValues) != -1) {
+            matched = true;
+            return false;
+          }
+        });
+
+        // if matched is true the current .herb element is returned
+        return matched;
+
+      });
+    });
+
+    $('.herb').hide().filter($filteredResults).show();
+
   });
 
-  /*
-
-/*
-$('.btn' ).click(function() {
-  var btnText = $(this).text();
-  $('#showClasses').replaceWith(btnText);
 });
-*/
-
-/*
-$('.btn').click(function( event ) {
-  var btnText = $(current).text();
-  $('#showClasses').replaceWith("new text");
-}); 
-*/
-
-
-
+ 
