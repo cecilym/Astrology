0950959ff374ad444b7c47b041b121d9dbae9c32
Cecily <cecilymadanes@gmail.com>
2020-05-12 13:05:02 -0400

    Add files via upload
    Merging the Astro Herbalism and Delineations projects, creating a link to switch between the two

--- herb-script.js
+++ herb-script.js
@@ -1,6 +1,5 @@
 $(document).ready(function () {
-  console.log( 'ready' );
-  $('.delineation').hide();
+  console.log( 'ready2!' );
   
   var $filterCheckboxes = $('input[type="checkbox"]');
 
@@ -19,18 +18,18 @@ $(document).ready(function () {
     });
 
     // create a collection containing all of the filterable elements
-    var $filteredResults = $('.delineation');
+    var $filteredResults = $('.herb');
 
     // loop over the selected filter name -> (array) values pairs
     $.each(selectedFilters, function(name, filterValues) {
 
-      // filter each .delineation element
+      // filter each .herb element
       $filteredResults = $filteredResults.filter(function() {
 
         var matched = false,
           currentFilterValues = $(this).data('category').split(' ');
 
-        // loop over each category value in the current .delineation' data-category
+        // loop over each category value in the current .herb's data-category
         $.each(currentFilterValues, function(_, currentFilterValue) {
 
           // if the current category exists in the selected filters array
@@ -43,13 +42,13 @@ $(document).ready(function () {
           }
         });
 
-        // if matched is true the current .delineation element is returned
+        // if matched is true the current .herb element is returned
         return matched;
 
       });
     });
 
-    $('.delineation').hide().filter($filteredResults).show();
+    $('.herb').hide().filter($filteredResults).show();
 
   });
 
